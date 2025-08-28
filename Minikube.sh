cce2726db3eef56f85e156a5843b0be2cf5d3eb6
Deepak Ram <154036627+Deepakram0929@users.noreply.github.com>
2025-08-28 14:26:20 +0530

    Update Minikube.sh

--- Minikube.sh
+++ Minikube.sh
@@ -1,14 +1,22 @@
+# Update the server
 sudo apt update -y
 sudo apt upgrade -y
+
+# Install docker
 sudo apt install curl wget apt-transport-https -y
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+# Install Minikube
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod +x /usr/local/bin/minikube
 sudo minikube version
+
+# Install Kubectl
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo kubectl version --client
 sudo minikube start --driver=docker --force
